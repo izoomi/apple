5109fa86901e39f46addad7212b3c65d854020f2
Izumi Wong-Horiuchi <izumiwongh@gmail.com>
2024-05-13 17:08:56 -0700

    Install Rspec

--- spec/requests/weather_spec.rb
+++ spec/requests/weather_spec.rb
@@ -1,11 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "Weathers", type: :request do
-  describe "GET /show" do
+  describe "GET /home" do
     it "returns http success" do
-      get "/weather/show"
+      get root_path
       expect(response).to have_http_status(:success)
     end
   end
 
+  describe "GET /search" do
+    it "returns http not found" do
+      get "/weather/search"
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
---
+++ spec/views/weather/search.html.erb_spec.rb
@@ -0,0 +1,4 @@
+require 'rails_helper'
+
+RSpec.describe "weather/search.html.erb", type: :view do
+end
